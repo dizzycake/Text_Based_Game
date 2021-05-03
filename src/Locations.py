6bcae045efde4c2468f1f6aa133edc667dcace87
dizzycake <44649723+dizzycake@users.noreply.github.com>
2021-05-03 08:56:34 -0600

    Add files via upload

---
+++ src/Locations.py
@@ -0,0 +1,32 @@
+class Locations:
+    def __init__(self, location, PImage, x, y,):
+        self.location = location
+        self.PImage = image1.jpeg
+        self.x = 0
+        self.y = 0
+        
+    def display(int x, int y)
+    
+campsite = Locations("Camp")
+forest = Locations("dfbhjfdhfds")
+PImage = Locations(x)
+x = Locations(0)
+y = Locations(0)
+print(campsite.location, forest.location)
+
+
+
+
+
+
+
+
+#CAMPSITE
+
+#FOREST
+
+#LAKE
+
+#CAVE
+
+#ROADSIDE
