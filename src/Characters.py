e21badc0c99f84189774225e4694bfb70ea69f01
KateNelson24 <80906139+KateNelson24@users.noreply.github.com>
2021-05-11 07:53:34 -0600

    Update Characters.py

--- src/Characters.py
+++ src/Characters.py
@@ -6,18 +6,18 @@
 class Characters:
     #moth = loadImage("MothMan.png")
     #nessie = loadImage("Nessie.png")
-    x = 0
-    y = 0
+    #x = 0
+    #y = 0
 
-def __init__(self, name, imageName , x, y):
-    self.name = name
-    self.imageName = imageName
-    nessie = loadImage(imageName)
-    self.x = x
-    self.y = y
+    def __init__(self, name, imageName, x, y):
+        self.name = name
+        self.imageName = imageName
+        nessie = loadImage(imageName)
+        self.x = x
+        self.y = y
 
-def display( image, x, y):
-    image(nessie, 75, 75)
+    def display(self, img):
+        n = image(img, 75, 75)
 
 # MOTHMAN
 
