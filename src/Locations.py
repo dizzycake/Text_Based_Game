8ef865a520f80e0e64327742ad9f70adf0e03932
KateNelson24 <80906139+KateNelson24@users.noreply.github.com>
2021-05-13 08:27:37 -0600

    Update Locations.py

--- src/Locations.py
+++ src/Locations.py
@@ -1,19 +1,15 @@
-class Locations(object):
-    image1 = loadImage("PImage0.png")
-    x = 0
-    y = 0
-
-    def __init__(self, name, imageName, x, y):
+class Locs:
+    
+    def __init__(self, name, imageName, x,y):
         self.name = name
         self.imageName = imageName
-        Campsite = loadImage(imageName)
-        Roadside = loadImage(imageName)
-        Forest = loadImage(imageName)
-        Lake = loadImage(imageName)
-        
+        camp = loadImage(imageName)
+        cave = loadImage(imageName)
+        forest = loadImage(imageName)
+        lake = loadImage(imageName)
         self.x = x
         self.y = y
 
-    def display(self, img):
-        n = image(img, 75, 75)
-
+    def display(self, loc):
+        l = image(loc, 300, 0)
+       
