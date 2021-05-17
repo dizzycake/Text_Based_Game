e821ede3ffa2c5fe1e92394100fd1da171ac8eb7
KateNelson24 <80906139+KateNelson24@users.noreply.github.com>
2021-05-16 18:52:41 -0600

    Update InteractBox.py

--- src/InteractBox.py
+++ src/InteractBox.py
@@ -9,3 +9,5 @@ p1 = Interact("John", 36, 'graphic')
 print(p1.name)
 print(p1.age)
 print(p1.image)
+
+#Mouse click 1, 2, 3 to initiate Hi, Reading, & Bookclub interactions, may or may not need a seperate class  
