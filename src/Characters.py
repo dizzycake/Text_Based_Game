e6a5154046b65acfdc1a84cdddade7ed898183e3
KateNelson24 <80906139+KateNelson24@users.noreply.github.com>
2021-05-19 08:31:22 -0600

    Update Characters.py

--- src/Characters.py
+++ src/Characters.py
@@ -1,12 +1,15 @@
 class Characters:
     
-    def __init__(self, name, imageName, x, y):
+    def __init__(self, name, imageName, x,y):
         self.name = name
         self.imageName = imageName
         nessie = loadImage(imageName)
+        moth = loadImage(imageName)
+        bf = loadImage(imageName)
+        yeti = loadImage(imageName)
         self.x = x
         self.y = y
 
     def display(self, img):
-        n = image(img, 75, 75)
-
+       n = image(img, 400, 120)
+       
