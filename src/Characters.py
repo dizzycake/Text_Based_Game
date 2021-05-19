471b6d8283ff8935d853ba03a6cfa4e05e017e6b
KateNelson24 <80906139+KateNelson24@users.noreply.github.com>
2021-05-19 07:47:10 -0600

    Update Characters.py

--- src/Characters.py
+++ src/Characters.py
@@ -1,14 +1,5 @@
-#moth = loadImage("MothMan.png")
-
-#bigf = loadImage("")
-#yeti = loadImage("")
-
 class Characters:
-    #moth = loadImage("MothMan.png")
-    #nessie = loadImage("Nessie.png")
-    #x = 0
-    #y = 0
-
+    
     def __init__(self, name, imageName, x, y):
         self.name = name
         self.imageName = imageName
@@ -19,12 +10,3 @@ class Characters:
     def display(self, img):
         n = image(img, 75, 75)
 
-# MOTHMAN
-
-
-# NESSY
-    #nessie = Characters(self, self.nessie, self.nessie, 75, 75)
-
-# BIGFOOT
-
-# YETI
